88e7648d9662a8c909b4fa1ca0ca89e8a9ce2037
Gandalf925 <77911973+Gandalf925@users.noreply.github.com>
2021-02-01 11:33:41 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -116,4 +116,239 @@
     console.log(text.split(' ')[0]);
 
 
+  const point = {x: 100, y: 180};
+
+  point.x = 200;
+  point['y'] = 300;
+  point.z = 90;
+
+  console.log(point);
+  console.log(point.x);
+  console.log(point['y']);
+  console.log(point.z)
+  console.log(point)
+  
+  delete point.z;
+  
+  console.log(point)
+
+
+  const otherProps = {
+    r:4,
+    color: 'red',
+    ...point
+  };
+  
+  console.log(otherProps);
+
+  const keys = Object.keys(point);
+  console.log(keys);
+
+  keys.forEach(key => console.log(`Key: ${key} Value: ${point[key]}`));
+
+  const points = [
+    {x: 100, y: 200},
+    {x: 13000, y: 260},
+    {x: 1000, y: 600},
+  ];
+
+  console.log(points[1].y);
+
+  let l = [1, 3];
+  let m = l;
+  l[0] = 5;
+  console.log(l);
+  console.log(m);
+  m = [...l];
+  m[0] = 100;
+  
+  console.log(l);
+  console.log(m)
+
+  // --------------
+
+  const str = 'hello';
+
+  console.log(str.length);
+
+  console.log(str.substring(2, 4));
+
+  const date = [2021, 2, 1];
+
+  console.log(date.join('/'));
+
+  const time = '17:08:34';
+
+  console.log(time.split(':'));
+
+  const [hour, minute, second] = time.split(':');
+
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
+
+
+  {
+    const scores = [10,3,9];
+    let sum = 0;
+    scores.forEach(score => sum += score);
+
+    const avg = sum / scores.length;
+
+    console.log(sum);
+    console.log(avg);
+
+    console.log(Math.floor(avg)); //少数以下削除
+    console.log(Math.ceil(avg)); //切り上げ
+    console.log(Math.round(avg)); //四捨五入
+    console.log(avg.toFixed(3)); //小数点以下3桁まで
+
+    console.log(Math.random());
+    console.log(Math.floor(Math.random() * 3) +1);
+
+    const day = new Date();
+    console.log(day);
+
+    console.log(day.getDate());
+    console.log(day.getDay());
+    console.log(day.getFullYear());
+    console.log(day.getMonth() +1);
+    console.log(day.getHours());
+    console.log(day.getMinutes());
+    console.log(day.getSeconds());
+
+  }
+
+  {
+    const d = new Date(1987, 8);
+    d.setHours(10,20,30);
+    // d.setDate(31);
+    d.setDate(d.getDate() + 3);
+    console.log(d);
+  }
+
+  {
+    // alert("HELLO!");
+    // const answer = confirm("Are you READY!?");
+    // answer === true ? alert("OK!"): alert("NOOOOOOO!");
+  }
+
+  {
+    // let i = 0;
+    // let x = 0;
+
+    // function showTime() {
+    //   console.log(new Date());
+    //   i++;
+    //   i > 2 ? clearInterval(intervalID): x++;
+    // }
+    // const intervalID = setInterval(showTime, 1000);
+  }
+
+
+
+  {
+    // let i = 0;
+    // function showTime() {
+    //   console.log(new Date());
+    //   const clearTimeId = setTimeout(showTime, 1000);
+    //   i++;
+    //   if(i > 2){
+    //     clearTimeout(clearTimeId);
+    //   }
+
+    // }
+
+    // showTime();
+  }
+
+  {
+    // const name = 'utsunomiya';
+    const name = 5;
+
+    try {
+      console.log(name.toUpperCase());
+    } catch (e){
+      console.log(e);
+    }
+    console.log('Finished!');
+  }
+
+  {
+    const post = [
+      {
+        text: 'JavaScriptの勉強中',
+        likeCount: 0,
+        show() {
+          console.log(`${this.text} - ${this.likeCount}いいね`);
+        }
+      },
+      {
+        text: '楽しかね。',
+        likeCount: 0,
+        show() {
+          console.log(`${this.text} - ${this.likeCount}いいね`);
+        }
+      }
+    ]
+
+    
+    post[0].show();
+    post[1].show();
+
+  }
+
+  {
+    class Toukou{
+      constructor(message){
+        this.message = message;
+        this.likeCount = 0;
+      }
+
+      show(){
+        console.log(`${this.message} - ${this.likeCount}いいね`);
+      }
+
+      like(){
+        this.likeCount++;
+      }
+
+      static info(){
+        console.log('Post class version 1.0');
+      }
+    }
+    
+    class SponsordToukou extends Toukou{
+      constructor(text,sponsor){
+        super(text);
+        this.sponsor = sponsor; 
+      }
+
+      show(){
+        super.show();
+        console.log(`...sponsored by ${this.sponsor}`);
+      }
+    }
+
+    const message = [
+      new Toukou('JavaScriptのクラス作成中'),
+      new Toukou('目が痛いわ'),
+      new SponsordToukou('お金が欲しいな',"ドットインストール"),
+    ];
+
+    message[0].show();
+    message[1].show();
+    message[1].like();
+    message[1].like();
+    message[0].show();
+    message[1].show();
+    Toukou.info();
+
+    message[2].show();
+
+    
+
+  }
 }
+
+
